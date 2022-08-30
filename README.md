7192051dd7aff1665aed2ef301cfbe089a43861f
Sargis Yonan <sargis@yonan.org>
2022-08-30 17:24:54 -0400

    added support for BDOL detection, and new diacritics

--- README.md
+++ README.md
@@ -2,23 +2,23 @@
 Assyrian text to IPA/latin transliterator
 
 Examples:
-```
-./syr2ipa.py ܘܟܠܹܐܠܹܗ ܥܲܠ ܣܹܠܵܐ ܕܝܵܡܵܐ. ܘܚܙܹܠܝܼ ܕܐَܣܸܩܠܹܗ ܕܵܒܵܐ ܡ̣ܢ ܝܵܡܵܐ؛
-IPA: wklele ʕal selɑ djɑmɑ wxzeli dsɪqle dɑbɑ mn jɑmɑ;  
-latin: wklehleh ʿal sehla dyama wkhzehlee dsiqleh daba mn yama;  
-```
 
+Latin:
 ```
-/syr2ipa.py ܓܵܘ ܠܸܠܝܼܵܐ ܓܵܘ ܐܝܼܡܵܡܵܐ                            
-IPA: gɑw lɪlɪɑ gɑw imɑmɑ  
-latin: gaw lilia gaw eemama
+./syr2ipa.py -lt 'ܘܟܠܹܐܠܹܗ ܥܲܠ ܣܹܠܵܐ ܕܝܵܡܵܐ. ܘܚܙܹܠܝܼ ܕܐ݇ܣܸܩܠܹܗ ܕܵܒܵܐ ܡ̣ܢ ܝܵܡܵܐ؛' 
+wklehleh ʿal sehla dyama. wkhzehli dsiqleh daba min yama; 
 ```
-
-This repository serves as a hosting site for various implementations of Syriac text to IPA/Latin transliterators in various programming languages.
+IPA:
+```
+./syr2ipa.py -t 'ܘܟܠܹܐܠܹܗ ܥܲܠ ܣܹܠܵܐ ܕܝܵܡܵܐ. ܘܚܙܹܠܝܼ ܕܐ݇ܣܸܩܠܹܗ ܕܵܒܵܐ ܡ̣ܢ ܝܵܡܵܐ؛' 
+wklele ʕal selɑ djɑmɑ. wxzelɪ dsɪqle dɑbɑ mɪn jɑmɑ;
+```
+With a specified Corpus Dictionary, BDOLs can be detected:
+```
+./syr2ipa.py -ld corpus.json -t 'ܘܟܠܹܐܠܹܗ ܥܲܠ ܣܹܠܵܐ ܕܝܵܡܵܐ. ܘܚܙܹܠܝܼ ܕܐ݇ܣܸܩܠܹܗ ܕܵܒܵܐ ܡ̣ܢ ܝܵܡܵܐ؛'
+wklehleh ʿal sehla d'yama. w'khzehlee d'siqleh daba min yama; 
+```
 
 Long term goals include:
 - A tunable consolidated .json character definition scheme that any of the implementations can look at for parsing
-- Python version (done)
-- C++ version
-- Javascript version (done)
-- Transliteration without vowels
+- Transliteration without vowels using the corpus dictionary
--- syr2ipa.py
+++ syr2ipa.py
@@ -9,6 +9,46 @@
 ##
 
 import sys
+import argparse
+import json
+
+parser = argparse.ArgumentParser(description='syr2ipa - Syriac to IPA transcriber')
+parser.add_argument('-t', '--text',
+                    help='Pass input text via this argument in stdin')
+parser.add_argument('-f', '--file',
+                    help='Read Syriac input from a text file')
+parser.add_argument('-o', '--output',
+                    help='Specify which file to write out the transcription to. Default is stdout.')
+parser.add_argument('-l', '--latin', action='store_true',
+                    help='Transcibe into latin phonetics in place of IPA')
+parser.add_argument('-d', '--dictionary',
+                    help='Load a JSON word dictionary to improve accuracy')
+parser.add_argument('-v', '--verbose', action='store_true',
+                    help='Enable verbose logging output')
+args = parser.parse_args()
+
+def IsBDOL(word, dictionary):
+    bdol_list = ['ܒ', 'ܕ', 'ܘ', 'ܠ']
+    vowels = ['ܲ', 'ܵ', 'ܸ', 'ܼ', 'ܿ', 'ܹ']
+    diacritics = ['݂', '݁', '̇', '̣', '̈', '݇', '̰', '̃', '̮']
+
+    if len(word) > 1:
+        if word[0] in bdol_list:
+            if word[1] not in vowels and word[1] not in diacritics:
+                if word[1:] in dictionary.keys():
+                    return True, word[1:]
+    return False, word
+
+def CleanUpWord(word):
+    letters = ['ܐ', 'ܑ', 'ܒ', 'ܓ', 'ܕ', 'ܖ', 'ܗ', 'ܘ', 'ܙ', 'ܚ', 'ܛ', 'ܝ', 'ܟ', 'ܠ', 'ܡ', 'ܢ', 'ܣ', 'ܤ', 'ܥ', 'ܦ', 'ܨ', 'ܩ', 'ܪ', 'ܫ', 'ܬ']
+    vowels = ['ܲ', 'ܵ', 'ܸ', 'ܼ', 'ܿ', 'ܹ']
+    diacritics = ['݂', '݁', '̇', '̣', '̈', '݇', '̰', '̃', '̮']
+    word = word.rstrip().lstrip()
+    newWord = ''
+    for char in word:
+        if char in letters or char in vowels or char in diacritics:
+            newWord += char
+    return newWord
 
 class SyrChar:
     ##
@@ -21,11 +61,13 @@ class SyrChar:
     def __init__(self, name, character, 
                 is_letter = False, 
                 is_matres = False, 
-                is_modifer = False, 
+                is_modifer = False,
+                is_qanuna = False,
                 modifier_function = None, 
                 is_vowel = False, 
                 is_punctuation = False,
                 is_talqana = False,
+                is_siyameh = False,
                 punctuation_override = '',
                 latin = '', ipa = ''):
         self.name = name
@@ -38,10 +80,12 @@ class SyrChar:
         self.punctuation_override = punctuation_override
         self.is_vowel = is_vowel
         self.is_talqana = is_talqana
+        self.is_qanuna = is_qanuna
+        self.is_siyameh = is_siyameh
         self.latin = latin
         self.ipa = ipa
 
-        if (self.is_modifer or self.is_matres or self.is_talqana) and self.modifier_function is None:
+        if (self.is_modifer or self.is_matres or self.is_talqana or self.is_qanuna or self.is_siyameh) and self.modifier_function is None:
             print(f'Error: defining a modifier: {n}, but not modifier function was definined - exiting'.format(n=self.name))
             exit(1)
 
@@ -67,11 +111,14 @@ class SyrChar:
 
 class SyrToken:
     def __init__(self, token):
+        self.nonsyr = None
         self.base = None
         self.modifier = None
         self.punctuation = None
         self.talqana = None
         self.vowel = None
+        self.qanuna = None
+        self.siyameh = None
 
         n_letter  = 0
         n_modifer = 0
@@ -82,6 +129,8 @@ class SyrToken:
                 if n_letter == 0:
                     self.base = t
                 n_letter += 1
+            elif t.name == 'NON-SYR-CHAR':
+                self.nonsyr = t
             elif t.is_punctuation:
                 self.punctuation = t
             elif t.is_modifer:
@@ -90,6 +139,10 @@ class SyrToken:
                 n_modifer += 1
             elif t.is_talqana:
                 self.talqana = t
+            elif t.is_qanuna:
+                self.qanuna = t
+            elif t.is_siyameh:
+                self.siyameh = t
             elif t.is_vowel:
                 if n_vowel == 0:
                     self.vowel = t
@@ -111,18 +164,26 @@ def SyrCharTokensToIPA(tokens):
         if t.base is None:
             if t.punctuation:
                 return t.punctuation.GetPunctuation()
-        
+
         next_token = None
         if t_itor + 1 <= n_tokens - 1:
             next_token = tokens[t_itor + 1]
 
         i = ''
         l = ''
-        if t.talqana:
+        
+        if t.nonsyr:
+            ipa += t.nonsyr.character
+            latin += t.nonsyr.character
+        elif t.talqana:
             i, l = t.talqana.Modify(t.base, previous_token, t, next_token)
         else:
-            if t.modifier:
+            if t.siyameh:
+                i, l = t.siyameh.Modify(t.base, previous_token, t, next_token)
+            elif t.modifier:
                 i, l = t.modifier.Modify(t.base, previous_token, t, next_token)
+            elif t.qanuna:
+                i, l = t.qanuna.Modify(t.base, previous_token, t, next_token)
             elif t.base.is_matres:
                 i, l = t.base.Modify(t.base, previous_token, t, next_token)
             else:
@@ -200,7 +261,6 @@ def ModifierMajliana(letter, previous_token, t, next_token):
 def ModifierSiyameh(letter, previous_token, t, next_token):
     ipa   = ''
     latin = ''
-
     if letter.character == 'ܖ':
         ipa = 'r'
         latin = 'r'
@@ -208,16 +268,17 @@ def ModifierSiyameh(letter, previous_token, t, next_token):
         ipa = letter.ipa
         latin = letter.latin
 
+    return ipa, latin
+
 def ModifierMatres(letter, previous_token, t, next_token):
     ipa   = ''
     latin = ''
-
     if letter.character == 'ܐ':
         if t.vowel:
             ipa = ''
             latin = ''
         elif previous_token is None:
-            if next_token and next_token.modifier and (next_token.modifier.name == 'KHWASA' or next_token.modifer.name == 'RWAKHA'):
+            if next_token and next_token.modifier and (next_token.modifier.name == 'KHWASA' or next_token.modifier.name == 'RWAKHA'):
                     ipa = ''
                     latin = ''
             else:
@@ -227,7 +288,7 @@ def ModifierMatres(letter, previous_token, t, next_token):
                 else:    
                     ipa = 'ɑ'
                     latin = 'a'
-        elif next_token is None or next_token.punctuation:
+        elif (next_token is None or next_token.base is None) or next_token.punctuation:
             if previous_token and previous_token.vowel and (previous_token.vowel.name == 'ZQAPPA' or previous_token.vowel.name == 'PTAKHA' or previous_token.vowel.name == 'ZLAMA_KIRYA' or previous_token.vowel.name == 'ZLAMA_YARIKHA'):
                 ipa = ''
                 latin = ''
@@ -240,9 +301,17 @@ def ModifierMatres(letter, previous_token, t, next_token):
             latin = letter.latin
 
     elif letter.character == 'ܗ':
-        if next_token is None and previous_token.vowel and (previous_token.vowel.name == 'ZQAPPA' or previous_token.vowel.name == 'PTAKHA' or previous_token.vowel.name == 'ZLAMA_KIRYA' or previous_token.vowel.name == 'ZLAMA_YARIKHA'):
+        if (next_token is None or next_token.base is None) and previous_token and previous_token.vowel and (previous_token.vowel.name == 'ZQAPPA' or previous_token.vowel.name == 'PTAKHA' or previous_token.vowel.name == 'ZLAMA_KIRYA' or previous_token.vowel.name == 'ZLAMA_YARIKHA'):
             ipa = ''
             latin = ''
+        else:
+            ipa = letter.ipa
+            latin = letter.latin
+
+    elif letter.character == 'ܝ':
+        if (previous_token is None or previous_token.base is None) and next_token.base and (next_token.base.character == 'ܘ' or next_token.base.character == 'ܗ'):
+            ipa = 'i'
+            latin = 'i'
         else:
             ipa = letter.ipa
             latin = letter.latin
@@ -250,8 +319,8 @@ def ModifierMatres(letter, previous_token, t, next_token):
     return ipa, latin
 
 def ModifierRukakha(letter, previous_token, t, next_token):
-    ipa   = ''
-    latin = ''
+    ipa   = letter.ipa
+    latin = letter.latin
 
     if letter.character == 'ܒ':
         ipa = 'w'
@@ -275,8 +344,8 @@ def ModifierRukakha(letter, previous_token, t, next_token):
     return ipa, latin
 
 def ModifierRukakhaSemiCircle(letter, previous_token, t, next_token):
-    ipa   = ''
-    latin = ''
+    ipa   = letter.ipa
+    latin = letter.latin
 
     if letter.character == 'ܦ':
         ipa = 'f'
@@ -286,6 +355,30 @@ def ModifierRukakhaSemiCircle(letter, previous_token, t, next_token):
 
 def ModifierTalqana(letter, previous_token, t, next_token):
     return '', ''
+
+def ModifierQanunaTop(letter, previous_token, t, next_token):
+    ipa   = letter.ipa
+    latin = letter.latin
+
+    if letter.character == 'ܡ' and t.vowel is None:
+        ipa = f'{ipa}ɑ'.format(ipa=letter.ipa)
+        latin = f'{latin}a'.format(latin=letter.latin)
+
+    if letter.character == 'ܗ' and previous_token and previous_token.vowel is None:
+        ipa = f'ɑ{ipa}'.format(ipa=letter.ipa)
+        latin = f'a{latin}'.format(latin=letter.latin)
+
+    return ipa, latin
+
+def ModifierQanunaBottom(letter, previous_token, t, next_token):
+    ipa   = letter.ipa
+    latin = letter.latin
+
+    if letter.character == 'ܡ' and t.vowel is None:
+        ipa = f'{ipa}ɪ'.format(ipa=letter.ipa)
+        latin = f'{latin}i'.format(latin=letter.latin)
+
+    return ipa, latin
 
 alap = SyrChar('ALAP', 'ܐ', is_letter = True, is_matres = True, modifier_function=ModifierMatres, latin = 'a', ipa = 'ʔ')
 alap_west = SyrChar('ALAP_WEST', 'ܐ', is_letter = True, latin = 'o', ipa = 'ʔ')
@@ -298,7 +391,7 @@ waw = SyrChar('WAW', 'ܘ', is_letter = True, latin = 'w', ipa = 'w')
 zain = SyrChar('ZAIN', 'ܙ', is_letter = True, latin = 'z', ipa = 'z')
 kheth = SyrChar('KHETH', 'ܚ', is_letter = True, latin = 'kh', ipa = 'x')
 theth = SyrChar('THETH', 'ܛ', is_letter = True, latin = 'ṭ', ipa = 'tˤ')
-yodh = SyrChar('YODH', 'ܝ', is_letter = True, latin = 'y', ipa = 'j')
+yodh = SyrChar('YODH', 'ܝ', is_letter = True, is_matres = True, modifier_function=ModifierMatres, latin = 'y', ipa = 'j')
 kap = SyrChar('KAP', 'ܟ', is_letter = True, latin = 'k', ipa = 'k')
 lammad = SyrChar('LAMMAD', 'ܠ', is_letter = True, latin = 'l', ipa = 'l')
 meem = SyrChar('MEEM', 'ܡ', is_letter = True, latin = 'm', ipa = 'm')
@@ -335,25 +428,28 @@ rukakha_semicircle  = SyrChar('RUKAKHA_SEMICIRCLE', '̮', is_modifer = True, mod
 majliana = SyrChar('MAJLIANA_BOTTOM', '̰', is_modifer = True, modifier_function = ModifierMajliana)
 majliana_top = SyrChar('MAJLIANA_TOP', '̃', is_modifer = True, modifier_function = ModifierMajliana)
 
-siyameh = SyrChar('SIYAMEH', '̈', is_modifer = True, modifier_function = ModifierSiyameh)
+siyameh = SyrChar('SIYAMEH', '̈', is_siyameh = True, modifier_function = ModifierSiyameh)
 talqana = SyrChar('TALQANA', '݇', is_talqana = True, modifier_function = ModifierTalqana)
 talqana_bottom = SyrChar('TALQANA_BOTTOM', '݈', is_talqana = True, modifier_function = ModifierTalqana)
 
-comma = SyrChar('COMMA', ',', is_punctuation = True)
+# comma = SyrChar('COMMA', ',', is_punctuation = True)
 syrcomma = SyrChar('SYRCOMMA', '،', is_punctuation = True, punctuation_override=',')
-semicolon = SyrChar('SEMICOMMA', ';', is_punctuation = True)
+# semicolon = SyrChar('SEMICOMMA', ';', is_punctuation = True)
 syrsemicolon = SyrChar('SYRSEMICOMMA', '؛', is_punctuation = True, punctuation_override=';')
-question = SyrChar('QUESTION', '?', is_punctuation = True)
+# question = SyrChar('QUESTION', '?', is_punctuation = True)
 syrquestion = SyrChar('SYRQUESTION', '؟', is_punctuation = True, punctuation_override='?')
-space = SyrChar('SPACE', ' ', is_punctuation = True)
+# space = SyrChar('SPACE', ' ', is_punctuation = True)
+syrcolon = SyrChar('SYRCOLON', '܃', is_punctuation = True, punctuation_override='.')
+qanuna_top = SyrChar('QANUNA_TOP', '̇', is_qanuna = True, modifier_function=ModifierQanunaTop)
+qanuna_bottom = SyrChar('QANUNA_BOTTOM', '̣', is_qanuna = True, modifier_function=ModifierQanunaBottom)
 
-EastSyrChars = [alap, beth, gammal, gammal_garshuni, dalath, heh, waw, zain, kheth, theth, yodh, kap, lammad, meem, nun, simkat, simkat_final, ain, peh, sadeh, qop, resh, dotless_resh, shin, taw, zqappa, ptakha, zlama_kirya, zlama_yarikha, pthaha_top, pthaha_bottom, zqapha_top, zqapha_bottom, rwasa_top, hwasa_bottom, hwasa_top, esasa_bottom, esasa_top, khwasa, rwakha, rukakha, rukakha_semicircle, majliana, majliana_top, siyameh, talqana, talqana_bottom, comma, syrcomma, semicolon, syrsemicolon, question, syrquestion, space]
-WestSyrChars = [alap_west, beth, gammal, gammal_garshuni, dalath, heh, waw, zain, kheth, theth, yodh, kap, lammad, meem, nun, simkat, simkat_final, ain, peh, sadeh, qop, resh, dotless_resh, shin, taw, zqappa, ptakha, zlama_kirya, zlama_yarikha, pthaha_top, pthaha_bottom, zqapha_top, zqapha_bottom, rwasa_top, hwasa_bottom, hwasa_top, esasa_bottom, esasa_top, khwasa, rwakha, rukakha, rukakha_semicircle, majliana, majliana_top, siyameh, talqana, talqana_bottom, comma, syrcomma, semicolon, syrsemicolon, question, syrquestion, space]
+EastSyrChars = [alap, beth, gammal, gammal_garshuni, dalath, heh, waw, zain, kheth, theth, yodh, kap, lammad, meem, nun, simkat, simkat_final, ain, peh, sadeh, qop, resh, dotless_resh, shin, taw, zqappa, ptakha, zlama_kirya, zlama_yarikha, pthaha_top, pthaha_bottom, zqapha_top, zqapha_bottom, rwasa_top, hwasa_bottom, hwasa_top, esasa_bottom, esasa_top, khwasa, rwakha, rukakha, rukakha_semicircle, majliana, majliana_top, siyameh, talqana, talqana_bottom, syrcomma, syrsemicolon, syrquestion, syrcolon, qanuna_top, qanuna_bottom]
+# WestSyrChars = [alap_west, beth, gammal, gammal_garshuni, dalath, heh, waw, zain, kheth, theth, yodh, kap, lammad, meem, nun, simkat, simkat_final, ain, peh, sadeh, qop, resh, dotless_resh, shin, taw, zqappa, ptakha, zlama_kirya, zlama_yarikha, pthaha_top, pthaha_bottom, zqapha_top, zqapha_bottom, rwasa_top, hwasa_bottom, hwasa_top, esasa_bottom, esasa_top, khwasa, rwakha, rukakha, rukakha_semicircle, majliana, majliana_top, siyameh, talqana, talqana_bottom, comma, syrcomma, semicolon, syrsemicolon, question, syrquestion, qanuna_top, qanuna_bottom]
 SyrChars = EastSyrChars
 
 def PrintSyrCharArray(syrChars):
     for char in syrChars:
-        print(char.name)
+        print(f'{char.name}: {char.character}'.format())
 
 def TokenizeLettersWithModifiers(SyrChars):
     tokens = []
@@ -364,6 +460,15 @@ def TokenizeLettersWithModifiers(SyrChars):
             if len(tok) > 0:
                 tokens.append(SyrToken(tok))
                 tok = []
+        elif char.name == 'NON-SYR-CHAR':
+            if len(tok) > 0:
+                tokens.append(SyrToken(tok))
+                tok = []
+            
+            tokens.append(SyrToken([char]))
+            
+            continue
+
         tok.append(char)
     
     if len(tok) > 0:
@@ -372,34 +477,111 @@ def TokenizeLettersWithModifiers(SyrChars):
     return tokens
 
 def StrToSyrChars(word):
+    thisChar = None
     syrChars = []
     for char in word:
         for syrChar in SyrChars:
             if char == syrChar.character:
-                syrChars.append(syrChar)
+                thisChar = syrChar
+        if thisChar is None:
+            # not a known syr char
+            thisChar = SyrChar('NON-SYR-CHAR', char)
+        syrChars.append(thisChar)
+        thisChar = None
+
     return syrChars
 
-def SyrWordStrToIPA(syrWord):
-    syrCharArray = StrToSyrChars(syrWord)
-    # PrintSyrCharArray(syrCharArray)
-    tokens = TokenizeLettersWithModifiers(syrCharArray)
-    return SyrCharTokensToIPA(tokens)
-
-def ProcessSyrText(syrStr):
-    space_split_text = syrStr.split(' ')
+def SyrStrStrToIPA(syrStr, dictionary=None):
+    spaceSplit = syrStr.split(' ')
     ipa = ''
     latin = ''
-    for word in space_split_text:
-        i,l = SyrWordStrToIPA(word)
+    for word in spaceSplit:
+        if dictionary:
+            is_bdol, _ = IsBDOL(CleanUpWord(word), dictionary)
+        else:
+            is_bdol = False
+
+        if is_bdol:
+            word = word[0] + "'" + word[1:]
+
+        syrCharArray = StrToSyrChars(word)
+        if args.verbose:
+            PrintSyrCharArray(syrCharArray)
+        tokens = TokenizeLettersWithModifiers(syrCharArray)
+        i,l = SyrCharTokensToIPA(tokens)
         ipa += i + ' '
         latin += l + ' '
 
     return ipa, latin
 
 if __name__ == "__main__":
-    text = ''
-    for word in sys.argv[1:]:
-        text += word + ' '
+    inputFile = None
+    outputFile = None
+    dictionary = None
 
-    print(f'IPA: {ProcessSyrText(text)[0]}'.format())
-    print(f'latin: {ProcessSyrText(text)[1]}'.format())
+    if args.latin:
+        latinOutput = True
+
+    if args.dictionary:
+        try:
+            inputDict = open(args.dictionary, 'r')
+            dictionary = json.load(inputDict)
+            inputDict.close()
+        except:
+            print(f'Error: could not open dictionary file {args.dictionary}'.format())
+            exit(1)
+
+    if args.output:
+        try:
+            outputFile = open(args.output, 'w+')
+        except:
+            print(f'Error opening output file: {args.output}'.format())
+            exit(1)
+    
+    if args.text:
+        inputText = args.text
+
+        if args.verbose:
+            print(f'Input Text: {inputText}'.format())
+
+        if args.latin:
+            outputText = SyrStrStrToIPA(inputText, dictionary)[1]
+        else:
+            outputText = SyrStrStrToIPA(inputText, dictionary)[0]
+
+        if outputFile is None:
+            print(outputText)
+        else:
+            outputFile.write(outputText)
+            outputFile.close()
+
+    elif args.file:
+        try:
+            inputFile = open(args.file, 'r')
+        except:
+            print(f'Error opening input file: {args.file}'.format())
+            exit(1)
+
+        for line in inputFile:
+            if args.verbose:
+                print(f'Processing line from file: {line}'.format())
+
+            if args.latin:
+                outputText = SyrStrStrToIPA(line, dictionary)[1]
+            else:
+                outputText = SyrStrStrToIPA(line, dictionary)[0]
+            
+            if outputFile is None:
+                print(outputText)
+            else:
+                outputFile.write(outputText)
+
+    else:
+        print('Error: no input [t|f] specified')
+        exit(1)
+
+    if outputFile:
+        outputFile.close()
+
+    if inputFile:
+        inputFile.close()
